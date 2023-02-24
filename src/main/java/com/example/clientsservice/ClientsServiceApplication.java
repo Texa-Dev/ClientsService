7c764798a812d168cf1aee0235fc6a4baf4776a0
ЄВРОМАЙСТЕР <artemtroc26@gmail.com>
2023-02-24 19:15:38 +0200

    add ClientService

--- src/main/java/com/example/clientsservice/ClientsServiceApplication.java
+++ src/main/java/com/example/clientsservice/ClientsServiceApplication.java
@@ -1,9 +1,11 @@
 package com.example.clientsservice;
 
-import com.example.clientsservice.models.Address;
 import com.example.clientsservice.models.Client;
-import com.example.clientsservice.models.Phone;
+import com.example.clientsservice.models.adress.Country;
+import com.example.clientsservice.models.adress.Region;
 import com.example.clientsservice.repositories.ClientRepository;
+import com.example.clientsservice.repositories.CountryRepository;
+import com.example.clientsservice.services.data.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +21,10 @@ public class ClientsServiceApplication {
     @Autowired
     private ClientRepository clientRepository;
 
+    @Autowired
+    private ClientService clientService;
+    private CountryRepository countryRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(ClientsServiceApplication.class, args);
 
@@ -26,10 +32,15 @@ public class ClientsServiceApplication {
 
     @EventListener(ApplicationReadyEvent.class)
     public void onLoaded() {
-        Client client=new Client(0, "Surname", "Name", "Patronymic", LocalDate.now(),
-                "email@test.com", Client.Gender.MALE, null, null ,null );
-       client= clientRepository.save(client);
-        System.out.println(client);
+//        Client client=new Client(0, "Surname", "Name", "Patronymic", LocalDate.now(),
+//                "email@test.com", Client.Gender.MALE,
+//                null, null ,null );
+//       client= clientService.save(client);
+//        System.out.println(client);
+
+        Country country=new Country(0,"Ukraine",new Region());
+        country= countryRepository.save(country);
+        System.out.println(country);
     }
 
 }
